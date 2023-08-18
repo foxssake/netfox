7753db84d43a89ac4cb8e3918294eb987deab2b6
Tamás Gálffy <ezittgtx@gmail.com>
2023-08-19 00:59:53 +0200

    rest of docs

--- addons/netfox.noray/protocol-handler.gd
+++ addons/netfox.noray/protocol-handler.gd
@@ -1,13 +1,20 @@
 extends RefCounted
 class_name NorayProtocolHandler
+## This class parses incoming data from noray's protocol.
+##
+## Unless you're writing your own noray integration, [Noray] should cover most
+## use cases.
 
+## Emitted for every command parsed during a [method ingest] call.
 signal on_command(command: String, data: String)
 
 var _strbuf = ""
 
+## Resets the parser.
 func reset():
 	_strbuf = ""
 
+## Parse an incoming piece of data.
 func ingest(data: String):
 	_strbuf += data
 	if not _strbuf.contains("\n"):
