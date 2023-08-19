5cb93d6d1040663eb5f7216ea016c06fe9c5df38
Tamás Gálffy <ezittgtx@gmail.com>
2023-08-19 16:14:38 +0200

    Setup game with movement

---
+++ examples/forest-brawl/scripts/brawler-controller.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var jump_velocity = 4.5
+@export var input: BrawlerInput
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	if not input:
+		input = $Input
+
+	position = Vector3(0, 4, 0)
+
+	# TODO: What if the RollbackSynchronizer had a flag for this?
+	await get_tree().process_frame
+	$RollbackSynchronizer.process_settings()
+
+func _tick(delta, _t):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Jump
+	if input.movement.y > 0 and is_on_floor():
+		velocity.y = jump_velocity * input.movement.y
+
+	# Movement
+	var direction = Vector3(input.movement.x, 0, input.movement.z).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+	# Aim
+	# print("Aiming at %s" % [input.aim])
+	if input.aim:
+		transform = transform.looking_at(position + Vector3(input.aim.x, 0, input.aim.z), Vector3.UP, true)
+
+	# Apply movement
+	velocity *= NetworkTime.physics_factor
+	move_and_slide()
+	velocity /= NetworkTime.physics_factor
