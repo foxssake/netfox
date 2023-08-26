9e3aef08c450d53526634bd7b44b378b31b66351
Tamás Gálffy <ezittgtx@gmail.com>
2023-08-27 00:09:59 +0200

    feat(game): Displacement

--- examples/forest-brawl/scripts/displacer.gd
+++ examples/forest-brawl/scripts/displacer.gd
@@ -18,13 +18,12 @@ func _ready():
 func _tick(tick):
 	if birth_tick <= tick and tick < death_tick:
 		for body in get_overlapping_bodies():
-			if body.is_in_group("Brawlers") and \
-				body.is_multiplayer_authority() and \
-				NetworkRollback.is_simulated(body):
+			var displaceable = _find_displaceable(body)
+			if body.is_multiplayer_authority() and displaceable != null and NetworkRollback.is_simulated(body):
 				var diff: Vector3 = body.global_position - global_position
-				diff.y = 0
-				body.global_position += diff.normalized() * strength * NetworkTime.ticktime
-				print("[%s][%s] Displaced to %s" % [body.name, tick, body.global_position])
+				var f = clampf(1.0 / (1.0 + diff.length_squared()), 0.0, 1.0)
+				diff.y = max(0, diff.y)
+				displaceable.displace(diff.normalized() * strength * f)
 
 func _real_tick(delta, tick):
 	if tick >= death_tick:
@@ -32,3 +31,8 @@ func _real_tick(delta, tick):
 
 	if tick > despawn_tick:
 		queue_free()
+
+func _find_displaceable(node: Node) -> Displaceable:
+	for result in node.get_children().filter(func(it): return it is Displaceable):
+		return result
+	return null
