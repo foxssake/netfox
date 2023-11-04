b79cc966f13bb4a9f07748a27028b7223a024adc
Tamás Gálffy <ezittgtx@gmail.com>
2023-11-04 18:14:32 +0100

    Add fullscreen setting

---
+++ examples/forest-brawl/scripts/settings/fullscreen-checkbutton.gd
@@ -0,0 +1,7 @@
+extends CheckButton
+
+func _toggled(button_pressed):
+	if button_pressed:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
