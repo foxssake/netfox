c137467b3e8b204f490f7ff96e9c5a6ab0672402
Tamás Gálffy <ezittgtx@gmail.com>
2023-11-05 23:15:22 +0100

    reset demo code

--- examples/forest-brawl/scripts/play-random-3d.gd
+++ examples/forest-brawl/scripts/play-random-3d.gd
@@ -3,14 +3,12 @@ class_name PlayRandomStream3D
 
 @export var sounds: Array[AudioStream] = []
 
-var idx = 0
+static var idx = 0
 
 func _ready():
 	if autoplay:
 		play_random()
 
 func play_random():
-	# stream = sounds.pick_random()
-	stream = sounds[idx]
-	idx = (idx + 1) % sounds.size()
+	stream = sounds.pick_random()
 	play()
