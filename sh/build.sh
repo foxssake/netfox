f2b014c99d9397422650d12336fec32eb917346e
Tamás Gálffy <ezittgtx@gmail.com>
2023-11-10 10:21:16 +0100

    name fix

--- sh/build.sh
+++ sh/build.sh
@@ -3,17 +3,6 @@
 BOLD="$(tput bold)"
 NC="$(tput sgr0)"
 
-make_tag=false
-
-while (( "$#" )); do
-  if [[ "$1" == "--tag" ]]; then
-    echo "Will create tag"
-    make_tag=true
-  fi
-
-  shift
-done
-
 # Assume we're running from project root
 source sh/shared.sh
 
@@ -30,24 +19,17 @@ for addon in ${addons[@]}; do
 done
 
 # Build example game
-echo $BOLD"Building Forest Brawlers" $NC
+echo $BOLD"Building Forest Brawl" $NC
 mkdir -p build/linux
 mkdir -p build/win64
 
 echo "Building with Linux/X11 preset"
-godot --headless --export-release "Linux/X11" "build/linux/forest-brawlers.x86_64"
-zip -j "build/forest-brawlers.v${version}.linux.zip" build/linux/*
+godot --headless --export-release "Linux/X11" "build/linux/forest-brawl.x86_64"
+zip -j "build/forest-brawl.v${version}.linux.zip" build/linux/*
 
 echo "Building with Windows preset"
-godot --headless --export-release "Windows Desktop" "build/win64/forest-brawlers.exe"
-zip -j "build/forest-brawlers.v${version}.win64.zip" build/win64/*
-
-# Tag release
-if [ "$make_tag" = true ]; then
-  echo $BOLD"Tagging as v$version"$NC
-  git tag -a "v$version" -m "v$version"
-  git push --tags
-fi
+godot --headless --export-release "Windows Desktop" "build/win64/forest-brawl.exe"
+zip -j "build/forest-brawl.v${version}.win64.zip" build/win64/*
 
 # Cleanup
 echo $BOLD"Cleaning up" $NC
