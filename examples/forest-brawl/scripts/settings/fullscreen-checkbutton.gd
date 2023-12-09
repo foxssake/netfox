4057459c282cc4feec3a5f301103f2a35f9d156c
Tamás Gálffy <ezittgtx@gmail.com>
2023-12-09 17:44:26 +0100

    chore: Fix warnings

--- examples/forest-brawl/scripts/settings/fullscreen-checkbutton.gd
+++ examples/forest-brawl/scripts/settings/fullscreen-checkbutton.gd
@@ -1,7 +1,7 @@
 extends CheckButton
 
-func _toggled(button_pressed):
-	if button_pressed:
+func _toggled(toggle):
+	if toggle:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
