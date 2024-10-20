73475a7ebf56ac70cf2a9852c19a5f3f45103c49
Tamás Gálffy <ezittgtx@gmail.com>
2024-10-20 23:55:16 +0200

    stick to platforms, kinda

---
+++ examples/forest-brawl/scripts/moving-platform.gd
@@ -0,0 +1,28 @@
+extends AnimatableBody3D
+class_name MovingPlatform
+
+@export var speed: float = 2.
+@onready var _origin: Vector3 = global_position
+@onready var _target: Vector3 = $Target.global_position
+@onready var _distance: float = _origin.distance_to(_target)
+var _velocity: Vector3 = Vector3.ZERO
+
+func get_velocity() -> Vector3:
+	return _velocity
+
+func _ready():
+	NetworkRollback.on_prepare_tick.connect(_apply_tick)
+
+func _apply_tick(tick: int):
+	var previous_position = _get_position_for_tick(tick - 1)
+	global_position = _get_position_for_tick(tick)
+	
+	# TODO: Figure out drifting
+	_velocity = (global_position - previous_position) / NetworkTime.ticktime * .71875
+
+func _get_position_for_tick(tick: int):
+	var distance_moved = NetworkTime.ticks_to_seconds(tick) * speed
+	var progress = distance_moved / _distance
+	progress = pingpong(progress, 1)
+	
+	return _origin.lerp(_target, progress)
