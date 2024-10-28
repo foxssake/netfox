ae0695b4d595972db302b8f56840f3b5f9c88805
Jake Cattrall <krazyjakee@gmail.com>
2024-10-28 01:20:28 +0000

    simplify logic

--- addons/netfox.extras/state-machine/network-state-machine.gd
+++ addons/netfox.extras/state-machine/network-state-machine.gd
@@ -4,17 +4,13 @@ class_name NetworkedStateMachine
 
 ## A networked state machine that can be used to synchronize state between peers.
 
-@export var max_history_size: int = 10
+@export var state: StringName = "": set = _set_state
 
 signal state_changed(old_state: NetworkedState, new_state: NetworkedState)
 
 # Semi-private state property
-var _current_state: NetworkedState = null
-var _current_state_name: StringName = "": set = _set_current_state_name
+var _state_object: NetworkedState = null
 var _available_states: Dictionary = {}
-
-# History buffer to store states using tick numbers as keys
-var _state_history: Dictionary = {}
 
 # Use the ready function to gather all available states
 func _ready():
@@ -22,47 +18,36 @@ func _ready():
 		if child is NetworkedState:
 			_available_states[child.name] = child
 
-func _set_current_state_name(new_value: StringName):
-	_current_state_name = new_value
-	if _available_states.has(new_value):
-		_current_state = _available_states[new_value]
-
 # Method to set the state
-func set_state(new_state: StringName) -> void:
+func _set_state(new_state: StringName) -> void:
 	if !new_state:
 		return
 	
+	if state == new_state:
+		return
+	
+	if !_available_states.has(new_state):
+		return
+		
 	var new_state_class: NetworkedState = _available_states[new_state]
 	if !new_state_class:
 		return
 	
-	if _current_state:
-		if !new_state_class.can_enter(_current_state):
+	if _state_object:
+		if !new_state_class.can_enter(_state_object):
 			return
 	
-		_current_state.exit()
-
-		# Record the state with the current tick number
-		_state_history[NetworkRollback._tick] = _current_state_name
-
-		# Ensure history size does not exceed max_history_size
-		if _state_history.size() > max_history_size:
-			var keys = _state_history.keys()
-			keys.sort()
-			var oldest_key = keys[0]
-			_state_history.erase(oldest_key)
+		_state_object.exit()
 	
-	emit_signal("state_changed", _current_state_name, new_state)
-	_current_state_name = new_state
-	_current_state.enter()
+	state_changed.emit(state, new_state)
+	state = new_state
+	_state_object = new_state_class
+	_state_object.enter()
 
 # Callback during rollback tick
 func _rollback_tick(delta: float, tick: int, is_fresh: bool) -> void:
-	if NetworkRollback.is_rollback():
-		if _state_history.has(tick) and _state_history.has(tick - 1):
-			var previous_state = _state_history[tick - 1]
-			set_state(previous_state)
-			_state_history.erase(tick)
+	if _state_object:
+		_state_object.update(delta, tick, is_fresh)
 
-	if _current_state:
-		_current_state.update(delta, tick, is_fresh)
+func set_state(new_state: StringName) -> void:
+	state = new_state
