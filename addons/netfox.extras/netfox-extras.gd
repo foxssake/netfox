cee407a2faa86ae2d36d267ca3da6115ebf91ab5
Alberto Klocker <albertok@gmail.com>
2024-11-07 13:40:15 +1000

    Window auto tiler for running multiple instances from editor

--- addons/netfox.extras/netfox-extras.gd
+++ addons/netfox.extras/netfox-extras.gd
@@ -1,18 +1,67 @@
 @tool
 extends EditorPlugin
 
+const ROOT = "res://addons/netfox.extras"
+
 var SETTINGS = [
-	_NetfoxLogger.make_setting("netfox/logging/netfox_extras_log_level")
+	_NetfoxLogger.make_setting("netfox/logging/netfox_extras_log_level"),
+	#Window Tiler Settings
+	{
+		"name": "netfox/extras/auto_tile_windows",
+		"value": true,
+		"type": TYPE_BOOL
+	},
+	{
+		"name": "netfox/extras/screen",
+		"value": 0,
+		"type": TYPE_INT
+	},
+	{
+		"name": "netfox/extras/borderless",
+		"value": true,
+		"type": TYPE_BOOL
+	},
 ]
+
+const AUTOLOADS = [
+	{
+		"name": "WindowTiler",
+		"path": ROOT + "/window-tiler.gd"
+	}
+]
+
+class WindowTilerEditorDebugger extends EditorDebuggerPlugin:
+
+	func _has_capture(prefix):
+		return prefix == "tile-session"
+
+	func _capture(message, data, session_id):
+
+		if message == "tile-session:get_id":
+			get_session(session_id).send_message("tile-session:session_id", [{"id": session_id, "total": get_sessions().size()}])
+			return true
+
+
+var window_tiler = WindowTilerEditorDebugger.new()
 
 func _enter_tree():
 	for setting in SETTINGS:
 		add_setting(setting)
 
+	for autoload in AUTOLOADS:
+		add_autoload_singleton(autoload.name, autoload.path)
+
+	add_debugger_plugin(window_tiler)
+
 func _exit_tree():
 	if ProjectSettings.get_setting("netfox/general/clear_settings", false):
 		for setting in SETTINGS:
 			remove_setting(setting)
+
+	for autoload in AUTOLOADS:
+		remove_autoload_singleton(autoload.name)
+
+	remove_debugger_plugin(window_tiler)
 
 func add_setting(setting: Dictionary):
 	if ProjectSettings.has_setting(setting.name):
---
+++ addons/netfox.extras/window-tiler.gd
@@ -0,0 +1,63 @@
+extends Node
+
+
+func _ready() -> void:
+	EngineDebugger.register_message_capture("tile-session", _on_tile_session)
+	EngineDebugger.send_message.call_deferred("tile-session:get_id", [])
+
+
+func _on_tile_session(message: String, data: Array) -> bool:
+	if message == "session_id":
+		var info: Dictionary = data[0]
+		var id = info.get("id", -1)
+		var total = info.get("total", 0)
+		if total > 1:
+			for i in range(total):
+				if i == id:
+					tile_window(i, total)
+		return true
+	return false
+
+
+func tile_window(i: int, total: int) -> void:
+
+	print(" auto tile window: ", str(ProjectSettings.get_setting("netfox/extras/auto_tile_windows","not set")))
+
+	if not ProjectSettings.get_setting("netfox/extras/auto_tile_windows", true):
+		return
+
+	var screen = ProjectSettings.get_setting("netfox/extras/screen", 0)
+	var screen_rect = DisplayServer.screen_get_usable_rect(screen)
+
+	var window: Window = get_tree().get_root()
+	window.set_current_screen(screen)
+	window.borderless = ProjectSettings.get_setting("netfox/extras/borderless", true)
+
+	# Divide up the screen
+
+	var windows_per_row = int(ceil(sqrt(total)))
+	var windows_per_col = int(ceil(total / float(windows_per_row)))
+	var window_size = Vector2(
+		screen_rect.size.x / windows_per_row, screen_rect.size.y / windows_per_col
+	)
+
+	# Set up window size, don't take up more than 1/4 of the screen.
+	var max_window_width = screen_rect.size.x / 2
+	var max_window_height = screen_rect.size.y / 2
+
+	if window_size.x > max_window_width:
+		window_size.x = max_window_width
+	if window_size.y > max_window_height:
+		window_size.y = max_window_height
+
+	window.set_size(window_size)
+
+	# Position of the window based on index.
+
+	var row = i / windows_per_row
+	var col = i % windows_per_row
+
+	var x = screen_rect.position.x + col * window_size.x
+	var y = screen_rect.position.y + row * window_size.y
+
+	window.set_position(Vector2(x, y))
