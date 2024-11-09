68d0cf7bd72899a3e13684d7b1d2586ee8bf39a4
Alberto Klocker <albertok@gmail.com>
2024-11-09 14:32:37 +1000

    Added docs

---
+++ docs/netfox.extras/guides/window-tiler.gd
@@ -0,0 +1,19 @@
+# WindowTiler
+
+A developer convenience feature that automatcally tiles the launched windows when working from editor.
+
+Some features may not work depending on your desktop enviroment or Godot version.
+
+![Window Tiler](../assets/window-tiler.gif)
+
+
+## Configuration
+
+*Auto Tile Windows* Enables auto tiling from editor launches.
+
+*Screen* Which screen number to move and tile the windows to.
+
+*Borderless* Enable borderless mode to make the most out of the screen real estate.
+
+
+
