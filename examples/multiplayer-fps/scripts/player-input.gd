1322cc12e0f4272bcd2a9e891d12186556781ae4
Jake Cattrall <krazyjakee@gmail.com>
2024-11-27 13:56:43 +0000

    fps look and move

--- examples/multiplayer-fps/scripts/player-input.gd
+++ examples/multiplayer-fps/scripts/player-input.gd
@@ -1,26 +1,59 @@
-extends Node
+extends BaseNetInput
 class_name PlayerInputFPS
+
+@export var mouse_sensitivity: float = 1.0
 
 @onready var camera: Camera3D = $"../Head/Camera3D"
 
+# Config variables
+var is_setup: bool = false
+var override_mouse: bool = false
+
+# Input variables
+var mouse_rotation: Vector2 = Vector2.ZERO
+var look_angle: Vector2 = Vector2.ZERO
 var movement: Vector3 = Vector3.ZERO
 var fire: bool = false
 var jump: bool = false
 
-func _ready():
-	NetworkTime.before_tick_loop.connect(_gather)
-	if is_multiplayer_authority():
-		camera.current = true
-
+func _notification(what):
+	if what == NOTIFICATION_WM_WINDOW_FOCUS_IN:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		override_mouse = false
+		
+func _input(event: InputEvent) -> void:
+	if !is_multiplayer_authority(): return
+	
+	if event is InputEventMouseMotion:
+		mouse_rotation.y = event.relative.x * mouse_sensitivity
+		mouse_rotation.x = event.relative.y * mouse_sensitivity
+		
+	if event.is_action_pressed("escape"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		override_mouse = true
+		
 func _gather():
-	if not is_multiplayer_authority():
-		return
+	if !is_setup:
+		setup()
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var mx = Input.get_axis("ui_left", "ui_right")
-	var mz = Input.get_axis("ui_up", "ui_down")
+	var mx = Input.get_axis("move_west", "move_east")
+	var mz = Input.get_axis("move_north", "move_south")
 	movement = Vector3(mx, 0, mz)
 
 	jump = Input.is_action_pressed("move_jump")
 	fire = Input.is_action_pressed("weapon_fire")
+	
+	if override_mouse:
+		look_angle = Vector2.ZERO
+		mouse_rotation = Vector2.ZERO
+	else:
+		var delta: float = 1.0 / NetworkTime.tickrate
+		look_angle = Vector2(-mouse_rotation.y * delta, -mouse_rotation.x * delta)
+		mouse_rotation = Vector2.ZERO
+
+func setup():
+	is_setup = true
+	camera.current = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- examples/multiplayer-fps/scripts/player.gd
+++ examples/multiplayer-fps/scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 
 @onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 @onready var current_state_label: Label3D = $StateNameLabel3D
+@onready var input: PlayerInputFPS = $Input
+@onready var head: Node3D = $Head
 
 var color: Color:
 	get: return _color
@@ -33,6 +35,16 @@ func _rollback_tick(delta: float, tick: int, is_fresh: bool) -> void:
 	_force_update_is_on_floor()
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
+	# Handle look left and right
+	rotate_object_local(Vector3(0, 1, 0), input.look_angle.x)
+	
+	# Handle look up and down
+	head.rotate_object_local(Vector3(1, 0, 0), input.look_angle.y)
+	
+	head.rotation.x = clamp(head.rotation.x, -1.57, 1.57)
+	head.rotation.z = 0
+	head.rotation.y = 0
 
 func _force_update_is_on_floor():
 	var old_velocity = velocity
