c80f52797ed2c7b72adf11227f458a8ddc3797cc
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-07 13:22:37 +0100

    feat: RewindableStateMachine display callbacks (#343)

--- examples/multiplayer-state-machine/scripts/character-states/Move.gd
+++ examples/multiplayer-state-machine/scripts/character-states/Move.gd
@@ -4,7 +4,7 @@ extends RewindableState
 @export var input: PlayerInputStateMachine
 @export var speed = 5.0
 
-func enter(_previous_state, _tick):
+func display_enter(_previous, _t):
 	character.color = Color.RED
 
 func tick(delta, tick, is_fresh):
