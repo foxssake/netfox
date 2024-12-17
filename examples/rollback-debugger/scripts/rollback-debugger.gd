38195e3b5d7c02885591fd67e3084bbb76bbb745
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-17 18:46:00 +0100

    skipset + defaults

--- examples/rollback-debugger/scripts/rollback-debugger.gd
+++ examples/rollback-debugger/scripts/rollback-debugger.gd
@@ -7,7 +7,8 @@ extends Node
 @onready var rollback_tick_data := %"Rollback Tick Data" as LineEdit
 @onready var has_input_data := %"Has Input Data" as Label
 @onready var input_age_data := %"Input Age Data" as Label
-@onready var simset_data := %"Simset Data" as Label
+@onready var simset_list := %"Simset List" as ItemList
+@onready var skipset_list := %"Skipset List" as ItemList
 @onready var transmit_state_data := %"Transmit State Data" as Label
 
 # Tool buttons
@@ -37,6 +38,14 @@ func _ready():
 	# Render initial data
 	_render_data()
 
+	# Set defaults
+	input_history_data.text = """{
+		0: { "Input:movement": Vector2(1, 0) },
+		1: { "Input:movement": Vector2(1, 1) },
+		2: { "Input:movement": Vector2(1, 1) },
+		3: { "Input:movement": Vector2(0, 1) },
+	}"""
+
 	# Start NetworkTime but make sure it doesn't tick on its own
 	NetworkTime.start()
 	NetworkTime.set_process(false)
@@ -45,23 +54,23 @@ func _ready():
 	rollback_synchronizer._on_transmit_state.connect(func(state, tick):
 		transmit_state_data.text = "@%d: %s" % [tick, var_to_str(state)]
 	)
-	
+
 	nt_before_loop_button.pressed.connect(func():
 		NetworkTime.before_tick_loop.emit()
 	)
-	
+
 	nt_before_tick_button.pressed.connect(func():
 		NetworkTime.before_tick.emit(NetworkTime.ticktime, NetworkTime.tick)
 	)
-	
+
 	nt_on_tick_button.pressed.connect(func():
 		NetworkTime.on_tick.emit(NetworkTime.ticktime, NetworkTime.tick)
 	)
-	
+
 	nr_before_loop_button.pressed.connect(func():
 		NetworkRollback.before_loop.emit()
 	)
-	
+
 	nr_prepare_button.pressed.connect(func():
 		NetworkRollback.on_prepare_tick.emit(NetworkRollback.tick)
 		NetworkRollback.after_prepare_tick.emit(NetworkRollback.tick)
@@ -75,20 +84,20 @@ func _ready():
 	nr_record_button.pressed.connect(func():
 		NetworkRollback.on_record_tick.emit(NetworkRollback.tick)
 	)
-	
+
 	nr_after_loop_button.pressed.connect(func():
 		NetworkRollback.after_loop.emit()
 	)
-	
+
 	nt_after_tick_button.pressed.connect(func():
 		NetworkTime.after_tick.emit(NetworkTime.ticktime, NetworkTime.tick)
 	)
-	
+
 	nt_after_tick_loop_button.pressed.connect(func():
 		NetworkTime.after_tick_loop.emit()
 		NetworkTime._tick += 1
 	)
-	
+
 	run_nr_tick_button.pressed.connect(func():
 		NetworkRollback.before_loop.emit()
 		
@@ -104,7 +113,7 @@ func _ready():
 	advance_button.pressed.connect(func():
 		NetworkRollback._tick += 1
 	)
-	
+
 	# Update UI after any button press
 	var buttons = get_parent().find_children("*", "Button")
 	for button in buttons:
@@ -117,18 +126,21 @@ func _render_data():
 
 	state_history_data.text = _serialize_history(rollback_synchronizer._states)
 	input_history_data.text = _serialize_history(rollback_synchronizer._inputs)
-	
+
 	if rollback_synchronizer.has_input():
 		has_input_data.text = "true"
 		input_age_data.text = str(rollback_synchronizer.get_input_age())
 	else:
 		has_input_data.text = "false"
 		input_age_data.text = "?"
-	
-	simset_data.text = "\n".join(
-		rollback_synchronizer._simset.values()\
-		.map(func(node): return "\t" + node.name)
-	)
+
+	simset_list.clear()
+	for sim_entry in rollback_synchronizer._simset.values():
+		simset_list.add_item(str(sim_entry), null, false)
+
+	skipset_list.clear()
+	for skip_entry in rollback_synchronizer._skipset.values():
+		skipset_list.add_item(str(skip_entry), null, false)
 
 func _read_data():
 	if network_tick_data.text.is_valid_int():
@@ -142,7 +154,7 @@ func _read_data():
 
 func _serialize_history(history: Dictionary) -> String:
 	var result = PackedStringArray()
-	
+
 	for tick in history.keys():
 		result.append("\t%d: %s" % [tick, var_to_str(history[tick]).replace("\n", "")])
 
--- examples/rollback-debugger/scripts/subject.gd
+++ examples/rollback-debugger/scripts/subject.gd
@@ -7,7 +7,7 @@ extends Sprite2D
 
 func _rollback_tick(_dt, _t, _if):
 	if not rollback_synchronizer.has_input() or rollback_synchronizer.get_input_age() > 0:
-		rollback_synchronizer.ignore(self)
+		rollback_synchronizer.ignore_prediction(self)
 		return
 
 	position += input.movement.normalized() * move_speed
