7aea9ef33c8a873d0a92d7ebb43692e22c510204
Tamás Gálffy <ezittgtx@gmail.com>
2024-12-26 18:24:03 +0100

    chore: Improve scripts

--- sh/push-docs.sh
+++ sh/push-docs.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-BOLD="$(tput bold)"
-NC="$(tput sgr0)"
-
-# git config
-git config user.name "Fox's Sake CI"
-git config user.email "ci@foxssake.studio"
-
 # Assume we're running from project root
 source sh/shared.sh
 
 # Grab commit history
-echo $BOLD"Unshallowing commit history"$NC
+print $BOLD"Unshallowing commit history"$NC
 git fetch --unshallow --filter=tree:0
 
 # Figure out version
@@ -27,6 +20,6 @@ elif [ "$TAG" != "" ]; then
 fi;
 
 # Push version
-echo "Pushing version $BOLD$VERSION$NC"
-mike deploy --push "$VERSION"
+print -e "Pushing version $BOLD$VERSION$NC"
+# mike deploy --push "$VERSION"
 
