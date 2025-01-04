77539e4d796fc61076ebcf6293e8e9ef2b421df6
Tamás Gálffy <ezittgtx@gmail.com>
2025-01-04 20:29:11 +0100

    manual hit detect

--- examples/forest-brawl/scripts/bomb-projectile.gd
+++ examples/forest-brawl/scripts/bomb-projectile.gd
@@ -30,15 +30,16 @@ func _tick(delta, _t):
 	force_shapecast_update()
 	
 	# Find the closest point of contact
-	var collision_points = collision_result\
-		.filter(func(it): return it.collider != fired_by)\
-		.map(func(it): return it.point)
-	collision_points.sort_custom(func(a, b): return position.distance_to(a) < position.distance_to(b))
-
-	if not collision_points.is_empty() and not is_first_tick:
-		# Jump to closest point of contact
-		var contact = collision_points[0]
-		position = contact
+	var space := get_world_3d().direct_space_state
+	var query := PhysicsShapeQueryParameters3D.new()
+	query.motion = motion
+	query.shape = shape
+	query.transform = global_transform
+	
+	var hit_interval := space.cast_motion(query)
+	if hit_interval[0] != 1.0 or hit_interval[1] != 1.0:
+		# Move to collision
+		position += motion * hit_interval[1]
 		_explode()
 	else:
 		position += motion
