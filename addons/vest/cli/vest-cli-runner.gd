5dd2cb14a0bbdb0873d9a0ea4950df91a0e4c448
Tamás Gálffy <ezittgtx@gmail.com>
2025-01-06 22:55:42 +0100

    fxs

--- addons/vest/cli/vest-cli-runner.gd
+++ addons/vest/cli/vest-cli-runner.gd
@@ -5,7 +5,12 @@ extends Node
 func _ready():
 	# Run tests
 	var results := runner.run_tests()
+	var tap_string := runner.as_tap(results)
+	
 	var file := FileAccess.open("res://vest.log", FileAccess.WRITE)
-	file.store_string(runner.as_tap(results))
+	file.store_string(tap_string)
+	
+	print("Saved results to vest.log")
+	print(tap_string)
 
 	get_tree().quit(0 if runner.is_success(results) else 1)
