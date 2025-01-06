aa8a8e915bb78bb4d19d751825ed90ad97e8fd1c
Tamás Gálffy <ezittgtx@gmail.com>
2025-01-06 22:34:08 +0100

    fxs

--- addons/vest/vest-cli.gd
+++ addons/vest/vest-cli.gd
@@ -2,18 +2,7 @@ extends SceneTree
 class_name VestCLI
 
 func _init() -> void:
-	# Load manually to ensure script is present
-	print("Huh?")
-	var runner_script = preload("res://addons/vest/vest-runner.gd")
-	print("Loaded script:", runner_script)
-
-	# Create runner
-	var runner = runner_script.new()
-	root.add_child(runner)
-
-	# Run tests
-	var results := runner.run_tests()
-	var file := FileAccess.open("res://vest.log", FileAccess.WRITE)
-	file.store_string(runner.as_tap(results))
-
-	quit()
+	# Preload scene
+	var cli_scene := preload("res://addons/vest/cli/vest-cli.tscn") as PackedScene
+	var cli = cli_scene.instantiate()
+	root.add_child(cli)
