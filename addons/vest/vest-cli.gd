3f8f65e585c6be328535f5b4885f88a8f5b701ec
Tamás Gálffy <ezittgtx@gmail.com>
2025-01-06 22:38:19 +0100

    fxs

--- addons/vest/vest-cli.gd
+++ addons/vest/vest-cli.gd
@@ -2,6 +2,13 @@ extends SceneTree
 class_name VestCLI
 
 func _init() -> void:
+	# Wait for main loop?
+	for i in range(8192):
+		await create_timer(0.1).timeout
+		if Engine.get_main_loop() != null:
+			print("Main loop present after %d iterations" % [i])
+			break
+	
 	# Preload scene
 	var cli_scene := preload("res://addons/vest/cli/vest-cli.tscn") as PackedScene
 	var cli = cli_scene.instantiate()
