6dfd723d46d1545b50733bb3aea959e712cfb2e9
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-05-14 19:24:11 +0300

    implement Area2/3D functions

--- addons/netfox.extras/physics/network-area-2d.gd
+++ addons/netfox.extras/physics/network-area-2d.gd
@@ -14,13 +14,41 @@ signal rollback_area_exited(area: Area2D, tick: int)
 var _overlapping_bodies := _HistoryBuffer.new()
 var _overlapping_areas := _HistoryBuffer.new()
 
+
+## Returns the result of [code]Area2D.get_overlapping_areas()[/code] for [param tick]
+func rollback_get_overlapping_areas(tick: int) -> Array[Area2D]:
+	return _overlapping_areas.get_snapshot(tick) if _overlapping_areas.has(tick) else []
+
+## Returns the result of [code]Area2D.get_overlapping_bodies()[/code] for [param tick]
+func rollback_get_overlapping_bodies(tick: int) -> Array[Node2D]:
+	return _overlapping_bodies.get_snapshot(tick) if _overlapping_bodies.has(tick) else []
+
+## Returns the result of [code]Area2D.has_overlapping_areas()[/code] for [param tick]
+func rollback_has_overlapping_areas(tick: int) -> bool:
+	return rollback_get_overlapping_areas(tick).size() > 0
+
+## Returns the result of [code]Area2D.has_overlapping_bodies()[/code] for [param tick]
+func rollback_has_overlapping_bodies(tick: int) -> bool:
+	return rollback_get_overlapping_bodies(tick).size() > 0
+
+## Returns the result of [code]Area2D.overlaps_area(area)[/code] for [param tick]
+func rollback_overlaps_area(area: Area2D, tick: int) -> bool:
+	return rollback_get_overlapping_areas(tick).has(area)
+
+## Returns the result of [code]Area2D.overlaps_body(body)[/code] for [param tick]
+func rollback_overlaps_body(body: Node2D, tick: int) -> bool:
+	return rollback_get_overlapping_bodies(tick).has(body)
+
+
 func _notification(what: int):
 	if what == NOTIFICATION_READY:
 		NetworkTime.on_tick.connect(_tick)
 
+
 func _tick(_d: float, tick: int):
 	_update_bodies(tick)
 	_update_areas(tick)
+
 
 func _update_bodies(tick: int):
 	var current := self.get_overlapping_bodies()
