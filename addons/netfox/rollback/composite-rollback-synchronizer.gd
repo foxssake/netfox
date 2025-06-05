b5efc8181defa6eaca5e54255f35ab438a6c6a0f
Tamás Gálffy <ezittgtx@gmail.com>
2025-06-05 23:15:47 +0200

    cleanup

--- addons/netfox/rollback/composite-rollback-synchronizer.gd
+++ addons/netfox/rollback/composite-rollback-synchronizer.gd
@@ -257,33 +257,21 @@ func _ready() -> void:
 	process_settings.call_deferred()
 
 func _connect_signals() -> void:
-	NetworkTime.before_tick.connect(_apply_tick_state) # History
-	NetworkTime.after_tick.connect(_record_input) # History
 	NetworkTime.after_tick.connect(_transmit_input) # Transmit
-	NetworkTime.after_tick.connect(_trim_history) # History
 	NetworkRollback.before_loop.connect(_notify_resim) # Simulate
-	NetworkRollback.on_prepare_tick.connect(_restore_tick) # History
 	NetworkRollback.on_prepare_tick.connect(_prepare_tick_process) # Simulate
 	NetworkRollback.on_process_tick.connect(_process_tick) # Simulate
 	NetworkRollback.on_process_tick.connect(_push_simset_metrics) # Simulate ( post-process )
 	NetworkRollback.on_record_tick.connect(_broadcast_tick) # Transmit
-	NetworkRollback.on_record_tick.connect(_record_tick) # History
-	NetworkRollback.after_loop.connect(_apply_display_state) # History
 	NetworkRollback.after_loop.connect(_reset_resim) # Simulate
 
 func _disconnect_signals() -> void:
-	NetworkTime.before_tick.disconnect(_apply_tick_state)
-	NetworkTime.after_tick.disconnect(_record_input)
 	NetworkTime.after_tick.disconnect(_transmit_input)
-	NetworkTime.after_tick.disconnect(_trim_history)
 	NetworkRollback.before_loop.disconnect(_notify_resim)
-	NetworkRollback.on_prepare_tick.disconnect(_restore_tick)
 	NetworkRollback.on_prepare_tick.disconnect(_prepare_tick_process)
 	NetworkRollback.on_process_tick.disconnect(_process_tick)
 	NetworkRollback.on_process_tick.disconnect(_push_simset_metrics)
 	NetworkRollback.on_record_tick.disconnect(_broadcast_tick)
-	NetworkRollback.on_record_tick.disconnect(_record_tick)
-	NetworkRollback.after_loop.disconnect(_apply_display_state)
 	NetworkRollback.after_loop.disconnect(_reset_resim)
 
 func _notification(what: int) -> void:
@@ -335,16 +323,6 @@ func _notify_resim() -> void:
 	else:
 		# We own inputs, simulate from latest authorative state
 		NetworkRollback.notify_resimulation_start(_latest_state_tick)
-
-func _restore_tick(tick: int) -> void:
-	# Prepare state
-	#	Done individually by Rewindables ( usually Rollback Synchronizers )
-	#	Restore input and state for tick
-	var state := _states.get_history(tick)
-	var input := _inputs.get_history(tick)
-
-	state.apply(_property_cache)
-	input.apply(_property_cache)
 
 func _prepare_tick_process(tick: int) -> void:
 	# Save data for input prediction
@@ -501,76 +479,13 @@ func _send_full_state(tick: int, peer: int = 0) -> void:
 		NetworkPerformance.push_full_state(full_state_snapshot)
 		NetworkPerformance.push_sent_state(full_state_snapshot)
 
-func _should_record_tick(tick: int) -> bool:
-	if _get_recorded_state_props().is_empty():
-		# Don't record tick if there's no props to record
-		return false
-
-	if _get_recorded_state_props().any(func(pe):
-		return NetworkRollback.is_mutated(pe.node, tick - 1)):
-		# If there's any node that was mutated, there's something to record
-		return true
-
-	# Otherwise, record only if we don't have authoritative state for the tick
-	return tick > _latest_state_tick
-
-func _should_record_property(property_entry: PropertyEntry) -> bool:
-	if NetworkRollback.is_mutated(property_entry.node):
-		return true
-	if _skipset.has(property_entry.node):
-		return false
-	return true
-
-func _record_tick(tick: int):
-	# Record state for specified tick ( current + 1 )
-
-	# Check if any of the managed nodes were mutated
-	var is_mutated := _get_recorded_state_props().any(func(pe):
-		return NetworkRollback.is_mutated(pe.node, tick - 1))
-
-	var record_state := _PropertySnapshot.extract(_get_state_props_to_record(tick))
-	if record_state.size():
-		var merge_state := _states.get_history(tick - 1)
-		_states.set_snapshot(tick, merge_state.merge(record_state))
-
 func _push_simset_metrics():
 	# Push metrics
 	NetworkPerformance.push_rollback_nodes_simulated(_simset.size())
 
-func _get_state_props_to_record(tick: int) -> Array[PropertyEntry]:
-	if not _should_record_tick(tick):
-		return []
-	if _skipset.is_empty():
-		return _get_recorded_state_props()
-
-	var result: Array[PropertyEntry] = []
-	for property_entry in _get_recorded_state_props():
-		if NetworkRollback.is_mutated(property_entry.node, tick - 1):
-			result.append(property_entry)
-		elif not _skipset.has(property_entry.node):
-			result.append(property_entry)
-	return _get_recorded_state_props().filter(func(pe): return _should_record_property(pe))
-
 func _reset_resim() -> void:
 	_earliest_input_tick = NetworkTime.tick
 
-func _apply_display_state() -> void:
-	# Apply display state
-	var display_state := _states.get_history(NetworkRollback.display_tick)
-	display_state.apply(_property_cache)
-
-func _apply_tick_state(_delta: float, tick: int) -> void:
-	# Apply state for tick
-	var state = _states.get_history(tick)
-	state.apply(_property_cache)
-
-func _record_input(_dt: float, tick: int) -> void:
-	# Record input
-	if not _get_recorded_input_props().is_empty():
-		var input = _PropertySnapshot.extract(_get_recorded_input_props())
-		var input_tick: int = tick + NetworkRollback.input_delay
-		_inputs.set_snapshot(input_tick, input)
-		
 func _transmit_input(_dt: float, tick: int) -> void:
 	# Transmit input
 	if not _get_owned_input_props().is_empty():
@@ -579,12 +494,6 @@ func _transmit_input(_dt: float, tick: int) -> void:
 		var target_peer := 0 if enable_input_broadcast else root.get_multiplayer_authority()
 		if target_peer != multiplayer.get_unique_id():
 			_submit_input.rpc_id(target_peer, input_tick, input_data)
-
-func _trim_history(_dt: float, _t: int) -> void:
-	# Trim history
-	_states.trim()
-	_inputs.trim()
-	_freshness_store.trim()
 
 func _reprocess_settings() -> void:
 	if not _properties_dirty or Engine.is_editor_hint():
