c115e1cb50637d48f0683d2d57c0e791332e7a55
Tamás Gálffy <ezittgtx@gmail.com>
2025-09-28 19:46:52 +0200

    fxs

--- sh/setup-plantuml.sh
+++ sh/setup-plantuml.sh
@@ -11,5 +11,6 @@ print "Downloading plantuml"
 curl -LO https://github.com/plantuml/plantuml/releases/download/v1.2025.4/plantuml-mit-1.2025.4.jar
 
 print "Starting server"
-java -jar ./plantuml-mit-1.2025.4.jar -picoweb:8080:127.0.0.1 &
-echo $!
+if ! java -jar ./plantuml-mit-1.2025.4.jar -picoweb:8080:127.0.0.1 &; then
+  print "Server failed!"
+fi;
