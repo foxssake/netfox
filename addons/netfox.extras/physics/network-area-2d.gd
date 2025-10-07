6c34dd00caddbdaf41246ab698ff921a8bc889fd
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2025-10-07 21:43:33 +0300

    fix: update network area state during _after_prepare_tick, fix cast errors

--- addons/netfox.extras/physics/network-area-2d.gd
+++ addons/netfox.extras/physics/network-area-2d.gd
@@ -17,11 +17,21 @@ var _overlapping_areas := _HistoryBuffer.new()
 
 ## Returns the result of [method Area2D.get_overlapping_areas] at [param tick]
 func rollback_get_overlapping_areas(tick: int) -> Array[Area2D]:
-	return _overlapping_areas.get_snapshot(tick) if _overlapping_areas.has(tick) else []
+	if not _overlapping_areas.has(tick):
+		return []
+	
+	var areas: Array[Area2D] = []
+	areas.assign((_overlapping_areas.get_snapshot(tick) as _Set).values())
+	return areas
 
 ## Returns the result of [method Area2D.get_overlapping_bodies] at [param tick]
 func rollback_get_overlapping_bodies(tick: int) -> Array[Node2D]:
-	return _overlapping_bodies.get_snapshot(tick) if _overlapping_bodies.has(tick) else []
+	if not _overlapping_bodies.has(tick):
+		return []
+	
+	var bodies: Array[Node2D] = []
+	bodies.assign((_overlapping_bodies.get_snapshot(tick) as _Set).values())
+	return bodies
 
 ## Returns the result of [method Area2D.has_overlapping_areas] at [param tick]
 func rollback_has_overlapping_areas(tick: int) -> bool:
@@ -44,16 +54,19 @@ func _notification(what: int):
 	# Use notification instead of _ready, so users can write their own _ready 
 	# callback without having to call super()
 	if what == NOTIFICATION_READY:
-		NetworkTime.on_tick.connect(_tick)
+		NetworkRollback.after_prepare_tick.connect(_after_prepare_tick)
 
 
-func _tick(_d: float, tick: int):
+func _after_prepare_tick(tick: int):
 	_update_bodies(tick)
 	_update_areas(tick)
 
 
 func _update_bodies(tick: int):
-	var current := self.get_overlapping_bodies()
+	if not self.monitoring:
+		return
+	
+	var current := _Set.of(self.get_overlapping_bodies())
 	_overlapping_bodies.set_snapshot(tick, current)
 	
 	if not _overlapping_bodies.has(tick - 1):
@@ -61,21 +74,24 @@ func _update_bodies(tick: int):
 			rollback_body_entered.emit(body, tick)
 		return
 	
-	var prev: Array = _overlapping_bodies.get_snapshot(tick - 1)
-	
-	for body in current:
-		if not prev.has(body):
-			rollback_body_entered.emit(body, tick)
-	
-	for body in prev:
-		if not current.has(body):
-			rollback_body_exited.emit(body, tick)
+	var prev: _Set = _overlapping_bodies.get_snapshot(tick - 1)
+	if not prev.equals(current):
+		for body in current:
+			if not prev.has(body):
+				rollback_body_entered.emit(body, tick)
+		
+		for body in prev:
+			if not current.has(body):
+				rollback_body_exited.emit(body, tick)
 	
 	_overlapping_bodies.trim(NetworkRollback.history_start)
 
 
 func _update_areas(tick: int):
-	var current := self.get_overlapping_areas()
+	if not self.monitoring:
+		return
+	
+	var current := _Set.of(self.get_overlapping_areas())
 	_overlapping_areas.set_snapshot(tick, current)
 	
 	if not _overlapping_areas.has(tick - 1):
@@ -83,14 +99,14 @@ func _update_areas(tick: int):
 			rollback_area_entered.emit(area, tick)
 		return
 	
-	var prev: Array = _overlapping_areas.get_snapshot(tick - 1)
-	
-	for area in current:
-		if not prev.has(area):
-			rollback_area_entered.emit(area, tick)
-	
-	for area in prev:
-		if not current.has(area):
-			rollback_area_exited.emit(area, tick)
+	var prev: _Set = _overlapping_areas.get_snapshot(tick - 1)
+	if not prev.equals(current):
+		for area in current:
+			if not prev.has(area):
+				rollback_area_entered.emit(area, tick)
+		
+		for area in prev:
+			if not current.has(area):
+				rollback_area_exited.emit(area, tick)
 	
 	_overlapping_areas.trim(NetworkRollback.history_start)
