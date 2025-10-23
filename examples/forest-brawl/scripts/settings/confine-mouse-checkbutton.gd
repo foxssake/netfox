f516d4b47fc43ed4f28a1eb810c1224ed5afae56
Tamás Gálffy <ezittgtx@gmail.com>
2025-10-23 12:12:24 +0200

    remove unused settings scripts

--- examples/forest-brawl/scripts/settings/confine-mouse-checkbutton.gd
+++
@@ -1,7 +0,0 @@
-extends CheckButton
-
-func _toggled(toggle):
-	if toggle:
-		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CONFINED)
-	else:
-		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE)
