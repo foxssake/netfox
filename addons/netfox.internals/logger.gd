4aecc9967b3a0b01d245f906138c8fd60e3edff6
Tamás Gálffy <ezittgtx@gmail.com>
2025-11-09 22:54:52 +0100

    doc comments

--- addons/netfox.internals/logger.gd
+++ addons/netfox.internals/logger.gd
@@ -1,37 +1,63 @@
 extends RefCounted
 class_name NetfoxLogger
 
+## Logger implementation for use with netfox
+##
+## NetfoxLoggers implement distinct log levels. These can be used to filter
+## which messages are actually emitted. All messages are output using [method 
+## @GlobalScope.print]. Warnings and errors are also pushed to the debug panel,
+## using [method @GlobalScope.push_warning] and [method @GlobalScope.push_error]
+## respectively.
+## [br][br]
+## Every logger has a name, and belongs to a module. Logging level can be
+## overridden per module, using [member module_log_level].
+## [br][br]
+## Loggers also support tags. Tags can be used to provide extra pieces of 
+## information that are logged with each message. Some tags are provided by
+## netfox. Additional tags can be added using [method register_tag].
+
+
 enum {
-	LOG_MIN,
-	LOG_TRACE,
-	LOG_DEBUG,
-	LOG_INFO,
-	LOG_WARN,
-	LOG_ERROR,
-	LOG_MAX
+	LOG_TRACE,	## Trace logs, the most verbose level
+	LOG_DEBUG,	## Debug logs
+	LOG_INFO,	## Info logs
+	LOG_WARN,	## Warnings
+	LOG_ERROR	## Errors
 }
 
+## Default log level to fall back on, if not configured
 const DEFAULT_LOG_LEVEL := LOG_DEBUG
 
 const _LEVEL_PREFIXES: Array[String] = [
-	"",
 	"TRC",
 	"DBG",
 	"INF",
 	"WRN",
 	"ERR",
-	""
 ]
 
+## Global logging level, used by all loggers
 static var log_level: int
+
+## Per-module logging level, used only by loggers belonging to the given module
+## [br][br]
+## This is a dictionary that associates module names ( strings ) to log levels
+## ( int, e.g. [constant LOG_DEBUG] ).
 static var module_log_level: Dictionary
 
 static var _tags: Dictionary = {}
 static var _ordered_tags: Array[Callable] = []
 
+## Logger module
 var module: String
+## Logger name
 var name: String
 
+
+## Register a tag
+## [br][br]
+## Tags are callables that provide pieces of context, included in all log
+## messges. The [param tag] callable must return a string.
 static func register_tag(tag: Callable, priority: int = 0) -> void:
 	# Save tag
 	if not _tags.has(priority):
@@ -49,6 +75,7 @@ static func register_tag(tag: Callable, priority: int = 0) -> void:
 		var tag_group = _tags[prio_group]
 		_ordered_tags.append_array(tag_group)
 
+## Free an already registered tag
 static func free_tag(tag: Callable) -> void:
 	for priority in _tags.keys():
 		var priority_group := _tags[priority] as Array
@@ -86,19 +113,34 @@ static func _make_setting(name: String) -> Dictionary:
 		"hint_string": "All,Trace,Debug,Info,Warning,Error,None"
 	}
 
+
 func _init(p_module: String, p_name: String):
 	module = p_module
 	name = p_name
 
+## Log a trace message
+## [br][br]
+## Traces are the most verbose, usually used for drilling down into very niche
+## bugs.
 func trace(text: String, values: Array = []):
 	_log_text(text, values, LOG_TRACE)
 
+## Log a debug message
+## [br][br]
+## Debug messages are verbose, usually used to reconstruct and investigate bugs.
 func debug(text: String, values: Array = []):
 	_log_text(text, values, LOG_DEBUG)
 
+## Log an info message
+## [br][br]
+## Info messages provide general notifications about application events.
 func info(text: String, values: Array = []):
 	_log_text(text, values, LOG_INFO)
 
+## Log a warning message
+## [br][br]
+## This is also forwarded to [method @GlobalScope.push_warning]. Warning
+## messages usually indicate that something has gone wrong, but is recoverable.
 func warning(text: String, values: Array = []):
 	if _check_log_level(LOG_WARN):
 		var formatted_text = _format_text(text, values, LOG_WARN)
@@ -106,6 +148,10 @@ func warning(text: String, values: Array = []):
 		# Print so it shows up in the Output panel too
 		print(formatted_text)
 
+## Log an error message
+## [br][br]
+## This is also forwarded to [method @GlobalScope.push_error]. Error messages
+## usually indicate an issue that can't be recovered from.
 func error(text: String, values: Array = []):
 	if _check_log_level(LOG_ERROR):
 		var formatted_text = _format_text(text, values, LOG_ERROR)
@@ -125,7 +171,7 @@ func _check_log_level(level: int) -> bool:
 	return true
 
 func _format_text(text: String, values: Array, level: int) -> String:
-	level = clampi(level, LOG_MIN, LOG_MAX)
+	level = clampi(level, LOG_TRACE, LOG_ERROR)
 	
 	var result := PackedStringArray()
 	
