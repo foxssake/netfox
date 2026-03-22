41ac590afdafdd0c54272dbcc2b189913cee2e48
Tamás Gálffy <ezittgtx@gmail.com>
2026-03-22 16:21:48 +0100

    integrate

--- addons/netfox/servers/rollback-liveness-server.gd
+++ addons/netfox/servers/rollback-liveness-server.gd
@@ -16,26 +16,33 @@ class_name _RollbackLivenessServer
 
 var _respawn_callback := {}
 var _despawn_callback := {}
-var _autofree_subjects := _Set.new()
+var _destroy_callback := {}
 
 var _spawn_tick := {}
 var _despawn_tick := {}
 var _applied_liveness := {}
 
-func register(subject: Node, respawn_callback: Callable, despawn_callback: Callable, autofree: bool = true, spawn_tick: int = NetworkRollback.tick) -> void:
+static var _logger := NetfoxLogger._for_netfox("RollbackLivenessServer")
+
+func register(subject: Node, respawn_callback: Callable, despawn_callback: Callable, destroy_callback: Callable = _free_subject.bind(subject), spawn_tick: int = NetworkRollback.tick) -> void:
+	if is_registered(subject):
+		_logger.warning("Re-registering subject: %s", [subject])
+		return
+
 	_respawn_callback[subject] = respawn_callback
 	_despawn_callback[subject] = despawn_callback
-
-	if autofree:
-		_autofree_subjects.add(subject)
+	_destroy_callback[subject] = destroy_callback
 
 	_applied_liveness[subject] = true
 	spawn(subject)
 
+func is_registered(subject: Node) -> bool:
+	return _has_subject(subject)
+
 func deregister(subject: Node) -> void:
 	_respawn_callback.erase(subject)
 	_despawn_callback.erase(subject)
-	_autofree_subjects.erase(subject)
+	_destroy_callback.erase(subject)
 	_spawn_tick.erase(subject)
 	_despawn_tick.erase(subject)
 
@@ -63,14 +70,20 @@ func restore_liveness(tick: int) -> void:
 
 func free_old_subjects(threshold_tick: int = NetworkRollback.history_start) -> void:
 	var old_subjects := []
-	for subject in _autofree_subjects:
+	for subject in _subjects():
 		if _despawn_tick.get(subject, threshold_tick + 1) < threshold_tick:
 			# Deregistering would modify _autofree_subjects, defer instead
 			old_subjects.append(subject)
 
 	for subject in old_subjects:
-		subject.queue_free()
+		_destroy_callback[subject].call()
 		deregister(subject)
 
 func _subjects() -> Array:
 	return _respawn_callback.keys()
+
+func _has_subject(subject: Object) -> bool:
+	return _respawn_callback.has(subject)
+
+func _free_subject(subject: Node) -> void:
+	subject.queue_free()
