2e2c4a0d8d8c940afa77b62125ed4f8c796ad673
Tamás Gálffy <ezittgtx@gmail.com>
2026-03-30 13:25:01 +0200

    ci

---
+++ sh/apidocs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source sh/shared.sh
+
+group "Ensuring dependencies"
+(cd sh/refdoc && bun i)
+endgroup
+
+group "Importing project"
+godot --headless --import .
+endgroup
+
+group "Dumping API docs"
+mkdir -p apidocs
+godot --doctool apidocs/ --no-docbase --gdscript-docs .
+endgroup
+
+group "Rendering reference pages"
+bun sh/refdoc/ apidocs/ ./ docs/class-reference/
+endgroup
+
+endgroup
