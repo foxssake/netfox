353711e4d3ea5035d39748f4d055789a1b95bfb0
Tamás Gálffy <ezittgtx@gmail.com>
2026-03-30 11:07:32 +0200

    some doc fixes

--- sh/refdoc/main.ts
+++ sh/refdoc/main.ts
@@ -1,9 +1,10 @@
 import { ClassDB } from "./src/classdb";
 import { MarkdownRenderer } from "./src/renderer";
 
-async function main() {
-  const dir = "../../apidocs/";
-  const src = "../../";
+async function main(args: string[]) {
+  const dir = args.at(0) ?? "apidocs/";
+  const src = args.at(1) ?? "./";
+  const out = args.at(2) ?? "out/"
 
   const classdb = (await ClassDB.fromDirectory(dir)).onlyNamedClasses();
 
@@ -13,7 +14,7 @@ async function main() {
 
   // const renderer = new MarkdownRenderer(classdb, false, false, c => c.srcPath?.startsWith("addons/netfox") === true);
   const renderer = new MarkdownRenderer(classdb);
-  renderer.render("out/");
+  renderer.render(out);
 }
 
-main();
+main(process.argv.slice(2));
