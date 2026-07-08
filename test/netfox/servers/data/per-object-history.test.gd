5a431b1606e941ca26d9bb0857c3da7eb8afce9f
Tamás Gálffy <ezittgtx@gmail.com>
2026-07-08 21:06:27 +0200

    lint

--- test/netfox/servers/data/per-object-history.test.gd
+++ test/netfox/servers/data/per-object-history.test.gd
@@ -8,27 +8,27 @@ func suite() -> void:
 		test("should ignore unknown subject", func():
 			var history := _PerObjectHistory.new(16)
 			var subject := Node.new()
-			
+
 			history.truncate_after(2, subject)
 			expect_does_not_contain(history.subjects(), subject)
-			
+
 			subject.queue_free()
 		)
-		
-		test("should truncate", func(): 
+
+		test("should truncate", func():
 			var history := _PerObjectHistory.new(16)
 			var subject := Node.new()
-			
+
 			history.set_property(0, subject, "hp", 12.)
 			history.set_property(1, subject, "hp", 16.)
 			history.set_property(2, subject, "hp", 20.)
 			history.set_property(3, subject, "hp", 24.)
-			
+
 			history.truncate_after(2, subject)
-			
+
 			expect_contains(history.subjects(), subject)
 			expect_equal(history.get_latest_tick(4, subject), 2)
-			
+
 			subject.queue_free()
 		)
 	)
